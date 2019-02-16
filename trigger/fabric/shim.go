31cd497ea4d206efbfaaad17a21f8fa7ffc2330a
Yueming Xu <yxucolo@gmail.com>
2019-02-15 21:06:04 -0700

    init TriggerMap

--- trigger/fabric/shim.go
+++ trigger/fabric/shim.go
@@ -3,3 +3,7 @@ package fabric
 // TriggerMap maps 'function' name in trigger handler setting to the trigger,
 // so we can lookup trigger by chaincode function name
 var TriggerMap map[string]*Trigger
+
+func init() {
+	TriggerMap = make(map[string]*Trigger)
+}
